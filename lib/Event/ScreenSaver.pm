b4b83596cd3c8323a37849959dc4ad7365c769b9
Ivan Wills <ivan.wills@gmail.com>
2009-07-10 16:59:23 +1000

    Added in code to run the screen saver checking

--- lib/Event/ScreenSaver.pm
+++ lib/Event/ScreenSaver.pm
@@ -1,6 +1,6 @@
 package Event::ScreenSaver;
 
-# Created on: 2009-07-08 05:19:38
+# Created on: 2009-07-08 05:33:45
 # Create by:  Ivan Wills
 # $Id$
 # $Revision$, $HeadURL$, $Date$
@@ -9,17 +9,72 @@ package Event::ScreenSaver;
 use Moose;
 use version;
 use Carp;
-use Scalar::Util;
-use List::Util;
-#use List::MoreUtils;
 use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
 
-our $VERSION     = version->new('0.0.1');
-our @EXPORT_OK   = qw//;
-our %EXPORT_TAGS = ();
-#our @EXPORT      = qw//;
+our $VERSION = version->new('0.0.1');
 
+has start => (
+	is  => 'rw',
+	isa => 'Sub',
+);
+has stop => (
+	is  => 'rw',
+	isa => 'Sub',
+);
+
+sub run {
+	my ($self) = @_;
+
+	eval { require Net::DBus };
+
+	if ($EVAL_ERROR) {
+		eval { require X11::Protocol };
+
+		die "You need to install eather Net::DBus or X11::Protocol\n" if $EVAL_ERROR;
+
+		require X11::Protocol::Ext::DPMS;
+
+		my $x = X11::Protocol->new();
+		$x->init_extension('DPMS');
+
+		my $power_level = '';
+		while (1) {
+			my $old_pl = $power_level;
+			($power_level, undef) = $x->DPMSInfo();
+			if( $old_pl eq 'DPMSModeOn' && $power_level ne 'DPMSModeOn' ) {
+				$self->start->() if $self->start;
+			}
+			elsif ( $power_level eq 'DPMSModeOn' && $old_pl ne 'DPMSModeOn' ) {
+				$self->stop->() if $self->stop;
+			}
+		}
+	}
+	else {
+		require Net::DBus::Reactor;
+
+		my $bus = Net::DBus->find;
+		my $screensaver = $bus->get_service("org.gnome.ScreenSaver");
+
+		my $screensaver_object = $screensaver->get_object("/org/gnome/ScreenSaver", "org.gnome.ScreenSaver");
+		$screensaver_object->connect_to_signal(
+			'ActiveChanged',
+			sub {
+				my $active = shift;
+				if ($active) {
+					$self->start->() if $self->start;
+				}
+				else {
+					$self->stop->() if $self->stop;
+				}
+			},
+		);
+
+		my $reactor = Net::DBus::Reactor->main();
+		$reactor->run();
+	}
+
+}
 
 1;
 
@@ -27,12 +82,12 @@ __END__
 
 =head1 NAME
 
-Event::ScreenSaver - <One-line description of module's purpose>
+Event::ScreenSaver - Provides the ability to hook functions to the starting
+and stopping of the screen saver (Linux only at the moment)
 
 =head1 VERSION
 
 This documentation refers to Event::ScreenSaver version 0.1.
-
 
 =head1 SYNOPSIS
 
@@ -42,68 +97,57 @@ This documentation refers to Event::ScreenSaver version 0.1.
    # This section will be as far as many users bother reading, so make it as
    # educational and exemplary as possible.
 
+   # create the screen saver object
+   my $ss = Event::ScreenSaver->new();
+
+   # add functions to events
+   $ss->on_start( sub {print "The screen saver started\n" } );
+   $ss->on_stop( sub { print "The screen saver stopped\n" } );
+
+   # run the event handler
+   $ss->run();
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
+This library provides an easy way to hook to the starting and stopping of
+the screen saver (currently only in Unix like environments).
 
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+The call back functions are passed the current event object.
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
+=head2 C<start ( [$sub] )>
 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
+Param: C<$sub> - sub - The starting call back function
 
-Name the section accordingly.
+Return: sub - The currently set starting function
 
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
+Description: Sets/Gets the function that will be called when the screen
+saver is started.
 
+=head2 C<stop ( [$sub] )>
 
+Param: C<$sub> - sub - The stopping call back function
+
+Return: sub - The currently set stopping function
+
+Description: Sets/Gets the function that will be called when the screen
+saver is stopped.
+
+=head2 C<run ()>
+
+This function starts the process for listening for screen saver events.
+It does not return.
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
@@ -114,7 +158,6 @@ Patches are welcome.
 =head1 AUTHOR
 
 Ivan Wills - (ivan.wills@gmail.com)
-<Author name(s)>  (<contact address>)
 
 =head1 LICENSE AND COPYRIGHT
 
